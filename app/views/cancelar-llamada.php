989edad2d63ec324c54e4420a9cb80360fe58e05
danielparrillas <daniel16parrillas@gmail.com>
2024-10-18 15:12:35 -0600

    feat: :sparkles: agregar vistas

---
+++ app/views/cancelar-llamada.php
@@ -0,0 +1,12 @@
+<?php
+$title = 'Cancelar llamada';
+require_once __DIR__ . '/components/header.php';
+date_default_timezone_set('America/El_Salvador');
+?>
+
+<body>
+	<?php require_once __DIR__ . '/components/navbar.php'; ?>
+	<main class="flex items-center justify-center">
+		<form action="/atender-llamada" method="POST" class="w-96">
+			<h2 class="text-center">Cancelar llamada</h2>
+		</form>
