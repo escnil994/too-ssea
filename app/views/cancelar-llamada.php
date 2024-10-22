7f663d0e1ec60ad5f50c58ae6c67be6d4baf0d18
natan5000 <natan823@hotmail.com>
2024-10-22 17:27:03 -0600

    Agregando funcionalidad de seguimiento y cancelacion de llamada

--- app/views/cancelar-llamada.php
+++ app/views/cancelar-llamada.php
@@ -7,6 +7,11 @@ date_default_timezone_set('America/El_Salvador');
 <body>
 	<?php require_once __DIR__ . '/components/navbar.php'; ?>
 	<main class="flex items-center justify-center">
-		<form action="/atender-llamada" method="POST" class="w-96">
+		<form action="/cancelar-llamada?id=<?php echo $llamada['id'] ?>" method="POST" class="w-96">
 			<h2 class="text-center">Cancelar llamada</h2>
+			<label>
+				Describa por que se cancelo la llamada
+				<textarea required placeholder="Comentario" name="comentario_cancel" rows="3"></textarea>
+			</label>
+			<button type="submit">Guardar</button>
 		</form>
